872507224b850494e585c2c19d8b2da2cd5c121c
Nikki Little <nikkilittle13@gmail.com>
2024-04-24 12:35:26 -0400

    completed lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,14 @@
 def write_file(file_name, file_content):
+    with open(str(file_name) + ".txt", mode="w") as file_name:
+        file_name.write(file_content)
     pass
 
 def append_file(file_name, append_content):
+    with open(str(file_name) + ".txt", mode="a") as file_name:
+        file_name.write(append_content)
     pass
 
 def read_file(file_name):
+    with open(str(file_name) + ".txt", mode="r") as file_name:
+        return file_name.read()
     pass
